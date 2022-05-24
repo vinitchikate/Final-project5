feae27070c02b01a95ae8385bf27eb6eb22a5eca
@52567_vinit <vinitchikate123@gmail.com>
2022-05-24 11:07:07 +0530

    group34

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const route = require('./routes/route');
+const multer= require("multer");
+const app = express();
+const { AppConfig } = require('aws-sdk');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use( multer().any())
+
+mongoose.connect("mongodb+srv://uranium:uranium@cluster0.pgmlm.mongodb.net/group34Database").then(() => console.log("MongoDB Is Connected")).catch(err => console.log(err));
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express App Is Running On Port ' + (process.env.PORT || 3000))
+});
